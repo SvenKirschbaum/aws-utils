5d0eb7fe9d5153964190a16373f7a2f1afd8eaa8
Sven Kirschbaum <sven@kirschbaum.me>
2025-03-01 22:24:55 +0100

    fix: Update roboto front imports

--- frontend/log-redirect/src/main.tsx
+++ frontend/log-redirect/src/main.tsx
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import '@fontsource/roboto';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
